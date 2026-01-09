c2d4206176a017f23a1f6e2c28caf790c792b4c4
SeOinm <anatjdls0716@gmail.com>
2026-01-10 01:56:14 +0900

    feat : 목표 세우기 페이지 수정

---
+++ src/pages/GoalNewConfirm/GoalNewConfirm.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import GoalNewForm from "../../components/GoalNewForm";
+
+const GoalNewConfirm = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-screen bg-[#FEF6EE]">
+      <header className="h-13 grid grid-cols-[1fr_auto_1fr] items-center bg-white border-b border-gray-100 px-4">
+        <div className="flex justify-start">
+          <button aria-label="back" onClick={() => navigate("/goal/new")} className="cursor-pointer">
+            <img src="/arrow-left.svg" alt="뒤로가기" />
+          </button>
+        </div>
+
+        <h1 className="text-[18px] font-semibold text-[#3F3E3A] text-center">목표 세우기</h1>
+      </header>
+
+      <GoalNewForm />
+    </div>
+  );
+};
+
+export default GoalNewConfirm;
